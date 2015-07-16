a82d220ec848d6ea79e58f46491a7572888f59b1
lilyyill <lillyclark@Studio-Manager-iMac-2.local>
2015-07-16 14:37:10 -0400

    C

---
+++ sticky.js
@@ -0,0 +1,11 @@
+var win      = $(window),
+    fxel     = $('nav'),
+    eloffset = fxel.offset().top;
+
+win.scroll(function() {
+    if (eloffset < win.scrollTop()) {
+        fxel.addClass("fixed");
+    } else {
+        fxel.removeClass("fixed");
+    }
+});
